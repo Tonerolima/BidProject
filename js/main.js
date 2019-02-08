a8e149bf79064459261e72e78f14bb53fa793ec9
Anthony Oyathelemhi <tonero91@gmail.com>
2019-02-08 21:18:45 +0100

    fix bug that affected toggling element visibility

--- js/main.js
+++ js/main.js
@@ -2,21 +2,41 @@ const projectSummary = document.querySelector(".project-summary");
 const projectSummary2 = document.querySelector(".project-summary-2");
 const requirements = document.querySelector(".requirements-list");
 const bundles = document.querySelector(".bundles");
+const bundleToggle = document.getElementById("bundle");
+const requirementsToggle = document.getElementById("requirements");
+
+const show = elements => {
+  elements.forEach(elem => {
+    elem.classList.remove("d-0");
+  });
+};
+
+const hide = elements => {
+  elements.forEach(elem => {
+    elem.classList.add("d-0");
+  });
+};
+
+const setActive = (elem, option) => {
+  if (option === "add") {
+    elem.classList.add("active");
+  } else {
+    elem.classList.remove("active");
+  }
+};
 
 document.querySelector(".requirements").addEventListener("click", e => {
   const clicked = e.target;
   clicked.classList.add("active");
   if (clicked.id === "bundle") {
-    document.getElementById("requirements").classList.remove("active");
-    projectSummary.classList.add("d-0");
-    requirements.classList.add("d-0");
-    projectSummary2.classList.remove("d-0");
-    bundles.classList.remove("d-0");
+    hide([projectSummary, requirements]);
+    show([projectSummary2, bundles]);
+    setActive(bundleToggle, "add");
+    setActive(requirementsToggle, "remove");
   } else if (clicked.id === "requirements") {
-    document.getElementById("bundle").classList.remove("active");
-    projectSummary.classList.remove("d-0");
-    requirements.classList.remove("d-0");
-    projectSummary2.classList.add("d-0");
-    bundles.classList.add("d-0");
+    show([projectSummary, requirements]);
+    hide([projectSummary2, bundles]);
+    setActive(requirementsToggle, "add");
+    setActive(bundleToggle, "remove");
   }
 });
